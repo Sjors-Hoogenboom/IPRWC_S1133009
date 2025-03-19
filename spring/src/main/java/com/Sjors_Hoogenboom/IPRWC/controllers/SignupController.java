7c9392fe6781b01cb1139988d234fb1e19daca1c
Sjors-Hoogenboom <SjorsH.2004@gmail.com>
2025-03-19 06:53:49 +0100

    Backend signupcontroller change

--- spring/src/main/java/com/Sjors_Hoogenboom/IPRWC/controllers/SignupController.java
+++ spring/src/main/java/com/Sjors_Hoogenboom/IPRWC/controllers/SignupController.java
@@ -5,11 +5,12 @@ import com.Sjors_Hoogenboom.IPRWC.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin
 @RestController
 @RequestMapping("/signup")
 public class SignupController {
@@ -22,13 +23,16 @@ public class SignupController {
     }
 
     @PostMapping
-    public ResponseEntity<String> signupCustomer(@RequestBody SignupRequest signupRequest) {
+    public ResponseEntity<Map<String, String>> signupCustomer(@RequestBody SignupRequest signupRequest) {
         boolean isUserCreated = authService.createCustomer(signupRequest);
+
+        Map<String, String> response = new HashMap<>();
         if (isUserCreated) {
-            return ResponseEntity.status(HttpStatus.CREATED).body("Customer created successfully");
-        }
-        else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to create customer");
+            response.put("message", "Customer created successfully");
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } else {
+            response.put("message", "Failed to create customer");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
 }
